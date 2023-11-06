a5bbc32bd2996557f6828ed19573ec12d37f3aa6
api-salari <147167298+api-salari@users.noreply.github.com>
2023-11-06 19:09:35 +0330

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,17 @@ function sendResponse(res, status, message) {
     res.setHeader("Content-Type", "application/json");
     res.status(status).send(JSON.stringify({ status, message }, null, 2));
 }
-
+app.get('/sendmessage', function(req, res) {
+const text = req.query.text;
+ const dataToSend = {
+      chat_id: "915303220",
+      text: $text 
+      };
+   axios.post('https://tapi.bale.ai/botwT9ArKZEC8Pxy7mSjvMPHsPj6JiJlIEQDX7P7MOT/sendMessage', dataToSend) 
+   res.send({
+   'status': 'ok',
+  });
+});
 app.get("/", async (req, res) => {
     const text = req.query.text;
 
