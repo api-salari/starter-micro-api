29a029a5e14df1b47e2f6bd56bf1ef5eaadb9d03
api-salari <147167298+api-salari@users.noreply.github.com>
2023-11-16 11:46:29 +0330

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function sendResponse(res, status, message) {
     res.status(status).send(JSON.stringify({ status, message }, null, 2));
 }
 
-app.get('/test', function(req, res) {
+app.get('/sendtext', function(req, res) {
   const text = req.query.text;  
   fs.appendFile("message.txt","سلام حروم زاده ای که کار نمیکنی", "utf-8");
   res.send({"text":"save"});
