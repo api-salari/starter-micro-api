7c0251886dcb8fe96b27786a640b40aed73c59cd
api-salari <147167298+api-salari@users.noreply.github.com>
2024-01-30 18:49:29 +0330

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,11 @@ app.get("/test2", async (req, res) => {
     }
     try {
         const result = await axios.post('https://lexica.art/api/infinite-prompts', {"text":"woman's ass","model":"lexica-aperture-v3.5","searchMode":"images","source":"search","cursor":100})
-        sendResponse(res, 200, result.data["prompts"]);
+        const arr = [];
+        for (const type of result.data["message"]) {  
+           arr.push(type["id"]);
+        }
+        sendResponse(res, 200, arr);
     } catch (error) {
         sendResponse(res, 403, "Error connecting to openai");
     }
