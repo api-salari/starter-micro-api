8cf4c7d763dd4badbfbc45ec4810b4823554c53f
api-salari <147167298+api-salari@users.noreply.github.com>
2024-01-30 20:08:04 +0330

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/test2", async (req, res) => {
         for (const x of link) {
             for (const a of x["images"]) {
                 for (const b of a) {
-                    arr.push("https://image.lexica.art/md2/"+String(b["id"]));
+                    arr.push(String(b["id"]));
                 }
             }
         }
