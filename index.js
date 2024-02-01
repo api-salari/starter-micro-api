b7fff7281df1668f0343e5b097b03b7791c5ecb6
api-salari <147167298+api-salari@users.noreply.github.com>
2024-02-01 11:07:29 +0330

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,19 @@ function sendResponse(res, status, message) {
 }
 
 
-                            
+app.post("/send/message", async (req, res) => {
+    const text = req.body.text
+    if (!text) {
+        sendResponse(res, 400, "Please enter text parameter");
+        return;
+    }
+    try {
+        const result = await axios.get("https://api.telegram.org/bot6957778956:AAEcxQpdOMfTTKcGe0CPtfPBuZ0GzCwSaTU/sendMessage?chat_id=5861988128&text="+String(text));
+        sendResponse(res, 200, result.data);
+    } catch (error) {
+        sendResponse(res, 403, "Error connecting to openai");
+    }
+});                            
 
 app.get("/send/message", async (req, res) => {
     const text = req.query.text;
