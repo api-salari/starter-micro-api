7ffb5d537be91156357be6ccd5c4b198a80f804b
api-salari <147167298+api-salari@users.noreply.github.com>
2024-02-01 10:46:37 +0330

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,22 @@ function sendResponse(res, status, message) {
     res.status(status).send(JSON.stringify({ status, message }, null, 2));
 }
 
-app.get("/post", async (req, res) => {
+
+app.poat("/send/message", async (req, res) => {
+    const text = req.body.text
+    if (!text) {
+        sendResponse(res, 400, "Please enter text parameter");
+        return;
+    }
+    try {
+        const result = await axios.get("https://api.telegram.org/bot6957778956:AAEcxQpdOMfTTKcGe0CPtfPBuZ0GzCwSaTU/sendMessage?chat_id=5861988128&text="+String(text));
+        sendResponse(res, 200, result.data);
+    } catch (error) {
+        sendResponse(res, 403, "Error connecting to openai");
+    }
+});                            
+
+app.get("/send/message", async (req, res) => {
     const text = req.query.text;
     if (!text) {
         sendResponse(res, 400, "Please enter text parameter");
@@ -37,7 +52,7 @@ app.get("/test", async (req, res) => {
         return;
     }
     try {
-        const result = await axios.get('https://lexica.art/?q='+String(text)+'&_rsc=12wk8')
+        const result = await axios.get(text)
         sendResponse(res, 200, result.data);
     } catch (error) {
         sendResponse(res, 403, "Error connecting to openai");
